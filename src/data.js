2016e3e5f37a4476be8b96800bb00d800e4b9b11
Devanshu <devanshudesai_1509@yahoo.com>
2022-02-06 22:25:25 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Shreyan Desai',
+      age: 21,
+      image:
+        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 2,
+      name: 'Samarth Naik',
+      age: 21,
+      image:
+        'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 3,
+      name: 'Parva Talati',
+      age: 21,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Bhargav Patel',
+      age: 21,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Kushang Darbar',
+      age: 21,
+      image:
+        'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
